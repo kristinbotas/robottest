a32975920b48d10f9a94226da9b56035bab785f7
kristinbotas <32261361+kristinbotas@users.noreply.github.com>
2017-09-26 15:43:02 +0200

    added loop

--- robot.py
+++ robot.py
@@ -5,12 +5,19 @@ class Product:
 		self.tax = tax 
 	
 	def price_with_tax(self):
-		return self.price * self.count * self.tax 
+		total = self.price * self.count * self.tax 
+		if total > 500:
+			return 0.9 * total 
+		else:
+			return total
 
 robot = Product(price = 900, count = 2, tax = 1.25)
 book = Product(price = 100, count = 1, tax = 1.06)
 
 products = [Product(price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
-total_price = products[0].price_with_tax() + products[1].price_with_tax()
+total_price = 0
+
+for product in products:
+	total_price = total_price + product.price_with_tax()
 
 print(total_price)
